50168a4a58ca72ff45fd5b29a11f6b1a0a5a2ea9
eddie.xie <oeddyo@gmail.com>
2014-04-23 17:27:40 -0400

    add

--- app/__init__.py
+++ app/__init__.py
@@ -24,6 +24,7 @@ app.gp.trainAndTest()
 print "before"
 import lib
 print 'testing info', lib.get_video_info("https://www.youtube.com/watch?v=NNihymK_XJA")
+video_id = "https://www.youtube.com/watch?v=NNihymK_XJA"
 ytfeed = app.yts.GetYouTubeVideoCommentFeed(video_id=video_id)
 contents = [my_e.content.text for my_e in ytfeed.entry]
 print 'printing comments...', contents
